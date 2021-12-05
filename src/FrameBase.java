5735691026211d355e7f96a682683f4cd35b552f
Lipatov1 <Ilya773@vk.com>
2021-12-05 20:48:14 +0400

    Лабораторная работа №6

--- src/FrameBase.java
+++ src/FrameBase.java
@@ -232,7 +232,6 @@ public class FrameBase extends JFrame {
                 JOptionPane.showMessageDialog(this, "Не удалось сохранить");
             }
         }
-
     }
 
     private void load() {
